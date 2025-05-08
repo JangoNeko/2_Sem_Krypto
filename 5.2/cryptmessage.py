5ae2d1b8de18d115335a9e4a2af1efacf74cfd7c
JangoNeko <lennart.korth@hotmail.de>
2025-05-08 22:24:32 +0200

    started working on bin

--- 5.2/cryptmessage.py
+++ 5.2/cryptmessage.py
@@ -1,6 +1,13 @@
-message = "According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible."
-iv =
+import random
+from random import randbytes
+message = b"According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible."
 def cypher(message):
-    msg = message.encode("utf-8")
+    iv = random.randbytes(16)
+    msg = message
     length = 16 - len(msg) % 16
     msg += bytes([length]) * length
+    plaintext = msg
+    print(plaintext)
+    return plaintext
+
+cypher(message)
